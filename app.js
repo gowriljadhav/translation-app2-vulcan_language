31d8fceabcd662e4b4e7b20e96203b2fc0b034e2
gowriljadhav <75152052+gowriljadhav@users.noreply.github.com>
2020-12-08 14:05:47 +0530

    js part done

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
+
+var translateButton=document.querySelector("#translate-button");
+var textInput=document.querySelector("#text-input");
+
+var outPut=document.querySelector("#output")
+
+// var  serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL+ "?" +"text="+text
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("Something wrong with server!! Try again after sometime.")
+}
+
+
+function clickEventHandler(){
+ //   outPut.innerText = "ajsajsajajkda "+ textInput.value;
+    var inputText=textInput.value; //taking input
+
+    //getting server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+
+    .then(json=>{
+        var translatedText=json.contents.translated;
+        outPut.innerText=translatedText; //output
+    })
+    .catch(errorHandler)
+
+
+};
+
+translateButton.addEventListener("click",clickEventHandler)
+
 // https://api.funtranslations.com/translate/shakespeare.json
